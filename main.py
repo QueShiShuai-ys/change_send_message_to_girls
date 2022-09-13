2e407154402fb86b273c69bcdcfc965222daa5f5
QueShiShuai-ys <113238768+QueShiShuai-ys@users.noreply.github.com>
2022-09-13 10:57:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -182,14 +182,14 @@ data = {
 }
 
 # 倒计时添加到数据
-for index, aim_date in enumerate(split_dates(end_date)):
-  key_name = "endday_left"
-  if index != 0:
-    key_name = key_name + "_%d" % index
-  data[key_name] = {
-    "value": get_counter_left(aim_date),
-    "color": get_random_color()
-  }
+#for index, aim_date in enumerate(split_dates(end_date)):
+#  key_name = "endday_left"
+#  if index != 0:
+#    key_name = key_name + "_%d" % index
+#  data[key_name] = {
+#    "value": get_counter_left(aim_date),
+#    "color": get_random_color()
+#  }
 
 # 各种正计时
 for index, aim_date in enumerate(split_dates(start_date)):
